8df323f18439550bede720cc7e6940be998e6f5e
Rory Campbell <roryc89@googlemail.com>
2024-06-07 17:58:12 +0200

    nextjs experiment

--- node/server/index.mjs
+++ node/server/index.mjs
@@ -1,15 +1,28 @@
-import 'dotenv/config';
-import express from 'express';
-import db from './db.mjs';
+import "dotenv/config";
+import express from "express";
+import db from "./db.mjs";
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (_req, res) => {
+  res.send("Hello World!");
+});
 
+app.get("/healthcheck", async (_req, res) => {
+  const healthcheck = {
+    uptime: process.uptime(),
+    message: "OK",
+    timestamp: Date.now(),
+  };
+  try {
+    res.send(healthcheck);
+  } catch (error) {
+    healthcheck.message = error;
+    res.status(503).send();
+  }
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
--- node/web/app/page.tsx
+++ node/web/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
 
-export default function Home() {
+
+export default function Home({ Component, pageProps }) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+      <Component {...pageProps} />
     </main>
-  );
+  )
 }
