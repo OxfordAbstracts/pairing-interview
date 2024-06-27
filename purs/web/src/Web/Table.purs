467dfd40b4883cf24d431ba4b8045b19c5e586d5
Rory Campbell <roryc89@googlemail.com>
2024-06-27 13:03:05 +0200

    fix

--- purs/web/src/Web/Table.purs
+++ purs/web/src/Web/Table.purs
@@ -2,39 +2,29 @@ module Web.Table where
 
 import Prelude
 
-import Control.Monad.Error.Class (try)
+import Control.Monad.Error.Class (class MonadError, catchError, try)
 import Data.Argonaut (Json, decodeJson, printJsonDecodeError)
-import Data.Array (intercalate, reverse, sortWith, take)
-import Data.Bifunctor (lmap)
 import Data.Either (Either(..))
 import Data.Maybe (Maybe(..))
 import Effect.Aff.Class (class MonadAff, liftAff)
+import Effect.Exception (Error)
 import Fetch (fetch)
 import Foreign (Foreign)
 import Halogen (liftAff)
 import Halogen as H
 import Halogen.HTML as HH
-import Halogen.HTML.Events as HE
 import Network.RemoteData (RemoteData(..))
 import Unsafe.Coerce (unsafeCoerce)
 import Web.HTML.Utils (css)
 
-data SortCol = Id | Title | Category | SubmitterName | SubmitterEmail
-data SortOrd = Asc | Desc
-data SortConf = SortConf SortCol SortOrd
-
-derive instance Eq SortCol
-derive instance Eq SortOrd
-
 data Action
   = Init
   | Receive Input
-  | SortBy SortCol
 
 type Input = {}
 
 type State =
-  { abstracts :: RemoteData String (Array Abstract), sortBy :: Maybe SortConf }
+  { abstracts :: RemoteData String (Array Abstract) }
 
 type Abstract =
   { title :: String
@@ -44,7 +34,7 @@ type Abstract =
   , email :: String
   }
 
-component :: forall output m q. MonadAff m => H.Component q Input output m
+component :: forall output m q. MonadError Error m => MonadAff m => H.Component q Input output m
 component =
   H.mkComponent
     { initialState
@@ -56,7 +46,7 @@ component =
     }
   where
   initialState :: Input -> State
-  initialState = const { abstracts: NotAsked, sortBy: Nothing }
+  initialState = const { abstracts: NotAsked }
 
   render :: State -> HH.HTML _ Action
   render state =
@@ -66,30 +56,14 @@ component =
               [ HH.tr []
                   [ HH.th
                       [ css "px-4 py-2"
-                      , HE.onClick $ const $ SortBy Id
-                      ]
-                      [ HH.text "Abstract ID" ]
-                  , HH.th
-                      [ css "px-4 py-2"
-                      , HE.onClick $ const $ SortBy Title
                       ]
                       [ HH.text "Title" ]
                   , HH.th
                       [ css "px-4 py-2"
-                      , HE.onClick $ const $ SortBy Category
                       ]
                       [ HH.text "Category" ]
                   ]
-              , HH.th
-                  [ css "px-4 py-2"
-                  , HE.onClick $ const $ SortBy SubmitterName
-                  ]
-                  [ HH.text "Submitter Name" ]
-              , HH.th
-                  [ css "px-4 py-2"
-                  , HE.onClick $ const $ SortBy SubmitterEmail
-                  ]
-                  [ HH.text "Submitter Email" ]
+
               ]
           ]
       , HH.tbody []
@@ -98,32 +72,37 @@ component =
             Loading -> [ HH.text "Loading..." ]
             Failure err -> [ HH.text err ]
             Success abstracts ->
-              let
-                sortFunc = identity
-              in
-                abstracts # sortFunc # take 100 <#> \abstract ->
-                  HH.tr []
-                    [ HH.td [ css "border px-4 py-2" ] [ HH.text abstract.title ]
-                    , HH.td [ css "border px-4 py-2" ] [ HH.text abstract.category ]
-                    ]
+              abstracts <#> \abstract ->
+                HH.tr []
+                  [ HH.td [ css "border px-4 py-2" ] [ HH.text abstract.title ]
+                  , HH.td [ css "border px-4 py-2" ] [ HH.text abstract.category ]
+                  ]
       ]
 
   handleAction :: Action -> H.HalogenM State Action _ _ m Unit
   handleAction = case _ of
     Init -> do
       H.modify_ _ { abstracts = Loading }
-      res <- liftAff $ fetch "http://localhost:8123/abstracts" {}
-      json <- liftAff $ toJson <$> res.json
-      H.modify_ _
-        { abstracts = case decodeJson json of
-            Left err -> Failure $ printJsonDecodeError err
-            Right abstracts -> Success abstracts
-        }
+      let
+        callApi = do
+          res <- fetch "http://localhost:8123/abstracts" {}
+          toJson <$> res.json
+
+      res <- liftAff $ try callApi
+
+      case res of
+        Left err -> do
+          H.modify_ _ { abstracts = Failure $ show err }
+
+        Right json ->
+          H.modify_ _
+            { abstracts = case decodeJson json of
+                Left err -> Failure $ printJsonDecodeError err
+                Right abstracts -> Success abstracts
+            }
 
     Receive input ->
       pure unit
-
-    SortBy sortCol -> pure unit
 
   toJson :: Foreign -> Json
   toJson = unsafeCoerce
